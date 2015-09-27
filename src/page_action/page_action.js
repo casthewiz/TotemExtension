4272abfd9248793de717ddc02404253d62936a82
Cassidy Schaufele <cschaufe@kent.edu>
2015-09-27 09:02:45 -0400

    ANOTHER PRODUCTION FREEZE

--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,8 +1,17 @@
 $('#generate').on("click", function() {
-    chrome.tabs.executeScript({file: '/src/inject/generate.js'})
+    $('#status').text = "Password generated!";
+
+    chrome.tabs.executeScript({file: "/js/jquery/jquery.min.js"}, function(){
+            chrome.tabs.executeScript({file: '/src/inject/generate.js'});
+    });
+    setTimeout(function(){$('#status').text = ' ' }, 3000);
 });
 
 
 $('#load').on("click", function() {
-    chrome.tabs.executeScript({file: '/src/inject/load.js'})
+    chrome.tabs.executeScript({file: "/js/jquery/jquery.min.js"}, function(){
+          chrome.tabs.executeScript({file: '/src/inject/load.js'});
+    });
+    $('#status').text = "Snap your totem now!";
+        setTimeout(function(){$('#status').text = ' ' }, 5000);
 });
