c3e6d909533d8f5d43867132c851869a1f830979
Cassidy Schaufele <cschaufe@kent.edu>
2015-09-26 22:06:27 -0400

    Increased input length, changed save to on form submit

--- src/inject/generate.js
+++ src/inject/generate.js
@@ -5,16 +5,18 @@ var websiteName = window.location.hostname;
 //random text, 8 char
 var text = "";
 var possible = "ABCHIJKLMNOPQRSTUVWXYZabcdefghijkrstuvwxyz0123456789!!!!!!!!!";
-for( var i=0; i < 18; i++ )
+for( var i=0; i < 32; i++ )
 	text += possible.charAt(Math.floor(Math.random() * possible.length));
+passwordInputs.val(text)
 
-
-$.get("https://totempass.herokuapp.com/create",
-      {userID: 1,
-       website: websiteName,
-			 password: text
-		 }).done(function(data) {
-			 console.log(data);
+$('form').submit(function(event) {
+	$.get("https://totempass.herokuapp.com/create",
+	      {userID: 1,
+	       website: websiteName,
+				 password: text
+			 }).done(function(data) {
+				 console.log(data);
+	})
 })
 
 console.log(passwordInputs.length);
