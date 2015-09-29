261b10682daec80a21ebbe3c06029a286340608b
Cassidy Schaufele <cschaufe@kent.edu>
2015-09-29 10:43:14 -0400

    Status messages

--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,10 +1,10 @@
 $('#generate').on("click", function() {
-    $('#status').text = "Password generated!";
+    $('#status').text("Password generated!");
 
     chrome.tabs.executeScript({file: "/js/jquery/jquery.min.js"}, function(){
             chrome.tabs.executeScript({file: '/src/inject/generate.js'});
     });
-    setTimeout(function(){$('#status').text = ' ' }, 3000);
+    setTimeout(function(){$('#status').text(' ')}, 3000);
 });
 
 
@@ -12,6 +12,6 @@ $('#load').on("click", function() {
     chrome.tabs.executeScript({file: "/js/jquery/jquery.min.js"}, function(){
           chrome.tabs.executeScript({file: '/src/inject/load.js'});
     });
-    $('#status').text = "Snap your totem now!";
-        setTimeout(function(){$('#status').text = ' ' }, 5000);
+    $('#status').text("Snap your totem now!");
+        setTimeout(function(){$('#status').text(' ') }, 5000);
 });
